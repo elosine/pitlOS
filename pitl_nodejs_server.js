7762ed336c6787313e7a1472b18632f7df7b4c4c
elosine <studiotttguttt>
2019-09-30 09:26:00 -0400

    Up

--- pitl_nodejs_server.js
+++ pitl_nodejs_server.js
@@ -6,6 +6,15 @@ var path = require('path');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
+//provide a sensible default for local development
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
+
+
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
